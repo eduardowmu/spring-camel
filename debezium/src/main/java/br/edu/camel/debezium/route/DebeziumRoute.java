eaf092a5ed8d6c3a1f71bf0cb71afda11a65b5ce
Eduardo Watanabe Murakoshi <eduardowmu@yahoo.com.br>
2023-05-16 12:23:33 -0300

    debezium parte 1 finalizado

--- debezium/src/main/java/br/edu/camel/debezium/route/DebeziumRoute.java
+++ debezium/src/main/java/br/edu/camel/debezium/route/DebeziumRoute.java
@@ -1,10 +1,10 @@
 package br.edu.camel.debezium.route;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spi.annotations.Component;
-//import org.springframework.stereotype.Component;
+//import org.apache.camel.spi.annotations.Component;
+import org.springframework.stereotype.Component;
 
-@Component("file/offset-file.dat")
+@Component
 public class DebeziumRoute extends RouteBuilder {
     /*Atributo obrigatorio para que o debizium seja ativado e consiga
     * definir uma rota. Se nao for encontrado, o proprio debizium cria*/
@@ -18,14 +18,14 @@ public class DebeziumRoute extends RouteBuilder {
     public void configure() throws Exception {
         /*"dbz-postgres" e a rota, obrigatoria, para conexao
         * Os dois parametros sao obrigatorios*/
-        from("debizium-postgres:dbz-postgres?offsetStorageFileName=" + OFFSET_STORAGE_FILE_NAME
+        from("debezium-postgres:dbz-postgres?offsetStorageFileName=" + OFFSET_STORAGE_FILE_NAME
                 + "&databaseHostName=" + HOST + "&databaseUser=" + USER_NAME
                 + "&databasePassword=" + PASSWORD + "&databaseServerName=" + DB
                 + "&databaseDbname=" + DB + "&pluginName=pgoutput")
         .log("EVENTO: ${body}")
         .log("identificador: ${headers.CamelDebeziumIdentifier}")
         .log("source metadata: ${headers.CamelDebeziumSourceMetaData}")
-        .log("operaçao: ${headers.CamelDebeziumOperation}")
+        .log("operaçao: ${headers.CamelDebeziumOperation}") //c = create, u = update, d = delete, r = snapshot
         .log("base: '${headers.CamelDebeziumSourceMetaData[db]}'")
         .log("tabela: '${headers.CamelDebeziumSourceMetaData[table]}'")
         .log("primary key: ${headers.CamelDebeziumKey}"); //decoderbufs e o padrao do debizium pg
