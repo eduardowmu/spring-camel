cc93ee12b67e2eb00168ec6e99b0b70ff3ce971b
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-27 01:10:38 -0300

    inital setup camel

---
+++ firlst-camel-app/src/main/java/com/edu/camel/firlst_camel_app/FirlstCamelAppApplication.java
@@ -0,0 +1,13 @@
+package com.edu.camel.firlst_camel_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirlstCamelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirlstCamelAppApplication.class, args);
+	}
+
+}
---
+++ firlst-camel-app/src/test/java/com/edu/camel/firlst_camel_app/FirlstCamelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.camel.firlst_camel_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirlstCamelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
