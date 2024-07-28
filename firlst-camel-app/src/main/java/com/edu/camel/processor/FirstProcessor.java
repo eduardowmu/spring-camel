cd69d6477c7334775e1aefc680a6b2533b440466
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-07-27 21:01:50 -0300

    from and to in route

---
+++ firlst-camel-app/src/main/java/com/edu/camel/processor/FirstProcessor.java
@@ -0,0 +1,11 @@
+package com.edu.camel.processor;
+
+import org.apache.camel.Processor;
+import org.apache.camel.Exchange;
+
+public class FirstProcessor implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+    }
+}
---
+++ firlst-camel-app/src/main/java/com/edu/camel/routes/FirstRoutes.java
@@ -0,0 +1,15 @@
+package com.edu.camel.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstRoutes extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer://test?period=5000")
+                .setBody(simple("Welcome!!"))
+                .to("log:test");
+    }
+}
