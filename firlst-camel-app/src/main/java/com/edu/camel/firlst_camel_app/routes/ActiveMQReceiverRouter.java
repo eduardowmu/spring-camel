77467a25ffed51729f8ff2994e4b0fc80e08ac32
Eduardo Watanabe <eduardo.watanabe@vilt.local>
2024-08-02 00:51:20 -0300

    consume and Âproducer activemq

---
+++ firlst-camel-app/src/main/java/com/edu/camel/firlst_camel_app/routes/ActiveMQReceiverRouter.java
@@ -0,0 +1,14 @@
+package com.edu.camel.firlst_camel_app.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQReceiverRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        //mesmo nome da fila criada: http://localhost:8161/admin/queues.jsp
+        from("activemq:test-amq")
+                .log("log:receiving msg => ${body}");
+    }
+}
---
+++ firlst-camel-app/src/main/java/com/edu/camel/firlst_camel_app/routes/ActiveMQSenderRouter.java
@@ -0,0 +1,14 @@
+package com.edu.camel.firlst_camel_app.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQSenderRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer://test?period=1000")
+                .setBody(simple("Welcome friend!"))
+                .to("activemq:test-mq");
+    }
+}
